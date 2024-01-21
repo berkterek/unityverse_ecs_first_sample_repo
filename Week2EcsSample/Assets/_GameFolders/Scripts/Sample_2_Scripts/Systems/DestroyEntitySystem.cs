954d2d4474eecac8b229baedd730a77ab99ac9eb
berkterek <42406263+berkterek@users.noreply.github.com>
2024-01-21 13:51:23 +0300

    feat: entity destroy system created

---
+++ Week2EcsSample/Assets/_GameFolders/Scripts/Sample_2_Scripts/Systems/DestroyEntitySystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+namespace Sample_2_Scripts
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateBefore(typeof(TransformSystemGroup))]
+    public partial struct DestroyEntitySystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var entityCommandBufferSystem = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+            var entityCommandBuffer = entityCommandBufferSystem.CreateCommandBuffer(state.WorldUnmanaged);
+            //WithEntityAccess() ile biz query'lerimize entity'de ekle demis oluyoruz
+            foreach (var (localTransform, moveData, entity) in SystemAPI.Query<RefRO<LocalTransform>, RefRO<MoveData>>().WithEntityAccess())
+            {
+                if(localTransform.ValueRO.Position.z < 10f) continue;
+                
+                entityCommandBuffer.DestroyEntity(entity);
+            }
+        }
+    }
+}
