3c676ccc99e066cdf369dae31daf316333549410
berkterek <42406263+berkterek@users.noreply.github.com>
2024-02-03 13:11:19 +0300

    refactor: dynamic buffer spawn system convert to job system

--- Week4Sample/Assets/_Scripts/Systems/EntitySpawnSystem.cs
+++ Week4Sample/Assets/_Scripts/Systems/EntitySpawnSystem.cs
@@ -6,38 +6,73 @@ using Unity.Transforms;
 
 namespace Sample1
 {
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
     public partial struct EntitySpawnSystem : ISystem
     {
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            
+            state.RequireForUpdate<EndInitializationEntityCommandBufferSystem.Singleton>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             float deltaTime = SystemAPI.Time.DeltaTime;
-            var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
-            foreach (var (spawnData, spawnEntityBufferDatas, randomData) in SystemAPI.Query<RefRW<SpawnData>, DynamicBuffer<SpawnEntityBufferData>, RefRW<RandomData>>())
+            var entityCommandBufferSystem =
+                SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>();
+            var ecb = entityCommandBufferSystem.CreateCommandBuffer(state.WorldUnmanaged);
+            // var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
+            // foreach (var (spawnData, spawnEntityBufferDatas, randomData) in SystemAPI.Query<RefRW<SpawnData>, DynamicBuffer<SpawnEntityBufferData>, RefRW<RandomData>>())
+            // {
+            //     spawnData.ValueRW.CurrentTime += deltaTime;
+            //     
+            //     if(spawnData.ValueRO.CurrentTime < spawnData.ValueRO.MaxSpawnTime) continue;
+            //
+            //     spawnData.ValueRW.CurrentTime = 0f;
+            //
+            //     var newEntity = entityCommandBuffer.Instantiate(spawnEntityBufferDatas[randomData.ValueRW.Random.NextInt(0, spawnEntityBufferDatas.Length)].Entity);
+            //     entityCommandBuffer.SetComponent(newEntity, new LocalTransform()
+            //     {
+            //         Position = spawnData.ValueRO.SpawnPosition,
+            //         Rotation = quaternion.identity,
+            //         Scale = 1f
+            //     });
+            // }
+            //
+            // entityCommandBuffer.Playback(state.EntityManager);
+            // entityCommandBuffer.Dispose();
+
+            new EntitySpawnJob()
             {
-                spawnData.ValueRW.CurrentTime += deltaTime;
-                
-                if(spawnData.ValueRO.CurrentTime < spawnData.ValueRO.MaxSpawnTime) continue;
+                DeltaTime = deltaTime,
+                Ecb = ecb.AsParallelWriter()
+            }.ScheduleParallel();
+        }
+    }
+
+    [BurstCompile]
+    public partial struct EntitySpawnJob : IJobEntity
+    {
+        public float DeltaTime;
+        public EntityCommandBuffer.ParallelWriter Ecb;
+        
+        [BurstCompile]
+        private void Execute(Entity entity, ref SpawnData spawnData, ref RandomData randomData, DynamicBuffer<SpawnEntityBufferData> spawnEntityBufferDatas, [ChunkIndexInQuery]int sortKey)
+        {
+            spawnData.CurrentTime += DeltaTime;
+
+            if (spawnData.CurrentTime < spawnData.MaxSpawnTime) return;
             
-                spawnData.ValueRW.CurrentTime = 0f;
+            spawnData.CurrentTime = 0f;
             
-                var newEntity = entityCommandBuffer.Instantiate(spawnEntityBufferDatas[randomData.ValueRW.Random.NextInt(0, spawnEntityBufferDatas.Length)].Entity);
-                entityCommandBuffer.SetComponent(newEntity, new LocalTransform()
-                {
-                    Position = spawnData.ValueRO.SpawnPosition,
-                    Rotation = quaternion.identity,
-                    Scale = 1f
-                });
-            }
-            
-            entityCommandBuffer.Playback(state.EntityManager);
-            entityCommandBuffer.Dispose();
+            var newEntity = Ecb.Instantiate(sortKey,spawnEntityBufferDatas[randomData.Random.NextInt(0, spawnEntityBufferDatas.Length)].Entity);
+            Ecb.SetComponent(sortKey,newEntity, new LocalTransform()
+            {
+                Position = spawnData.SpawnPosition,
+                Rotation = quaternion.identity,
+                Scale = 1f
+            });
         }
     }
 }
