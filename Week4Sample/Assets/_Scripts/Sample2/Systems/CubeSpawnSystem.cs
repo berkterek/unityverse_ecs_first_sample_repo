536202ae7605a6abded0f7d90dd69e56e922a9a1
berkterek <42406263+berkterek@users.noreply.github.com>
2024-02-04 12:32:57 +0300

    refactor: spawn system updated with schedule job system

--- Week4Sample/Assets/_Scripts/Sample2/Systems/CubeSpawnSystem.cs
+++ Week4Sample/Assets/_Scripts/Sample2/Systems/CubeSpawnSystem.cs
@@ -20,24 +20,39 @@ namespace Sample2
             var deltaTime = SystemAPI.Time.DeltaTime;
             var ecbSystem = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>();
             var ecb = ecbSystem.CreateCommandBuffer(state.WorldUnmanaged);
-            foreach (var (spawnTime, spawnEntityData, spawnPositionsReference, randomData) in SystemAPI.Query<RefRW<SpawnTimeData>, RefRW<SpawnEntityData>, RefRO<SpawnPositionsReference>, RefRW<RandomData>>())
-            {
-                spawnTime.ValueRW.CurrentSpawnTime += deltaTime;
-                if(spawnTime.ValueRO.CurrentSpawnTime < spawnTime.ValueRO.MaxSpawnTime) continue;
-                
-                spawnTime.ValueRW.CurrentSpawnTime = 0f;
 
-                var newEntity = ecb.Instantiate(spawnEntityData.ValueRW.Entity);
-                var randomIndex = randomData.ValueRW.Random.NextInt(0,
-                    spawnPositionsReference.ValueRO.BlobValueReference.Value.Values.Length);
-                var position = spawnPositionsReference.ValueRO.BlobValueReference.Value.Values[randomIndex];
-                ecb.SetComponent(newEntity, new LocalTransform()
-                {
-                    Position = position,
-                    Rotation = quaternion.identity,
-                    Scale = 1f
-                });
-            }
+            new CubeSpawnJob
+            {
+                Ecb = ecb,
+                DeltaTime = deltaTime
+            }.Schedule();
         }
     }
+
+    [BurstCompile]
+    public partial struct CubeSpawnJob : IJobEntity
+    {
+        public float DeltaTime;
+        public EntityCommandBuffer Ecb;
+        
+        [BurstCompile]
+        private void Execute(Entity entity, ref SpawnTimeData spawnTimeData, ref SpawnEntityData spawnEntityData, ref RandomData randomData, in SpawnPositionsReference spawnPositionsReference)
+        {
+            spawnTimeData.CurrentSpawnTime += DeltaTime;
+            if (spawnTimeData.CurrentSpawnTime < spawnTimeData.MaxSpawnTime) return;
+
+            spawnTimeData.CurrentSpawnTime = 0f;
+            
+            var newEntity = Ecb.Instantiate(spawnEntityData.Entity);
+            var randomIndex = randomData.Random.NextInt(0,
+                spawnPositionsReference.BlobValueReference.Value.Values.Length);
+            var position = spawnPositionsReference.BlobValueReference.Value.Values[randomIndex];
+            Ecb.SetComponent(newEntity, new LocalTransform()
+            {
+                Position = position,
+                Rotation = quaternion.identity,
+                Scale = 1f
+            });
+        }
+    } 
 }
