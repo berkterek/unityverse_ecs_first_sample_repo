6f62727f66065c227887029a75148a4c5aaebd1e
berkterek <42406263+berkterek@users.noreply.github.com>
2024-02-17 11:44:16 +0300

    feat: visual side for entity created

---
+++ Week6Sample/Assets/_Scripts/Controllers/SoldierVisualController.cs
@@ -0,0 +1,33 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Sample1
+{
+    public class SoldierVisualController : MonoBehaviour
+    {
+        [SerializeField] Animator _animator;
+        [SerializeField] Transform _transform;
+
+        void OnValidate()
+        {
+            if (_animator == null) _animator = GetComponentInChildren<Animator>();
+            if (_transform == null) _transform = GetComponent<Transform>();
+        }
+
+        public void MoveAnimation(float speed)
+        {
+            _animator.SetFloat(AnimatorConstHelper.Speed, speed);
+        }
+
+        public void SetPositionAndRotation(float3 position, quaternion rotation)
+        {
+            _transform.position = position;
+            _transform.rotation = rotation;
+        }
+    }
+
+    public class VisualAnimation
+    {
+        
+    }
+}
---
+++ Week6Sample/Assets/_Scripts/Helpers/AnimatorConstHelper.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Sample1
+{
+    public static class AnimatorConstHelper
+    {
+        public static readonly int Speed = Animator.StringToHash("Speed");
+    }    
+}
